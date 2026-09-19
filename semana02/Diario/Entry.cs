63e3386aca6736102d48603359080dc323267c3b
GaAntelmi <105023335+GaAntelmi@users.noreply.github.com>
2026-09-19 19:45:27 -0300

    Atividade semana 2

---
+++ semana02/Diario/Entry.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Entry
+{
+    public string _date;
+        public string _promptText;
+            public string _entryText;
+                public string _mood; // Campo extra para exceder os requisitos
+
+                    public void Display()
+                        {
+                                Console.WriteLine($"Data: {_date} - Pergunta: {_promptText}");
+                                        Console.WriteLine($"Humor do dia: {_mood}");
+                                                Console.WriteLine($"Resposta: {_entryText}\n");
+                                                    }
+                                                    }
+                                                    
